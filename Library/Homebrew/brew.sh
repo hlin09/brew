c5532cfbfd708fefa2c55a20317a66c976fcc2a5
Hao Lin <hlin@fb.com>
2017-11-12 21:47:34 -0800

    Comment out the root check

--- Library/Homebrew/brew.sh
+++ Library/Homebrew/brew.sh
@@ -282,7 +282,7 @@ As Homebrew does not drop privileges on installation you would be giving all
 build scripts full access to your system.
 EOS
 }
-check-run-command-as-root
+# check-run-command-as-root
 
 if [[ "$HOMEBREW_PREFIX" = "/usr/local" &&
       "$HOMEBREW_PREFIX" != "$HOMEBREW_REPOSITORY" &&
